cba9d0f1ee22f0899b2e93264d0d74bbe11a71a6
Volodymyr-Sadovskyi-2023 <139169228+Volodymyr-Sadovskyi-2023@users.noreply.github.com>
2023-08-18 11:51:14 +0300

    Add files via upload

---
+++ Task Epic 2/UserRegisterInterface.cs
@@ -0,0 +1,9 @@
+﻿namespace ChallengeMe.Tests
+{
+    public interface IUserService
+    {
+        void RegisterUser(string username, string password);
+        void EnterTheRegister(string username, string password);
+        void UnregisterUser(string username);
+    }
+}
