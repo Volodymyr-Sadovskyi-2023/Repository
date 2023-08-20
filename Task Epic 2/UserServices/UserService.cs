41a957cdb928da7919d83c435ebfddfb78f99537
Volodymyr-Sadovskyi-2023 <139169228+Volodymyr-Sadovskyi-2023@users.noreply.github.com>
2023-08-20 20:11:03 +0300

    Create UserService.cs

---
+++ Task Epic 2/UserServices/UserService.cs
@@ -0,0 +1,33 @@
+using ChallengeMe.CONFIDENT;
+
+namespace ChallengeMe.Tests
+{
+    public class UserService : IUserService
+    {
+        private readonly List<User> users = new List<User>();
+
+        public void EnterTheRegister(string username, string password)
+        {
+            var user = users.FirstOrDefault(u => u.Username == username && u.Password == password);
+
+            if (user != null)
+            {
+                Console.WriteLine("You have entered " + user.Username);
+            }
+            else
+            {
+                Console.WriteLine("You have not registered");
+            }
+        }
+
+        public void RegisterUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnregisterUser(string username)
+        {
+            UsersRegister.Users[0] = null;
+        }
+    }
+}
