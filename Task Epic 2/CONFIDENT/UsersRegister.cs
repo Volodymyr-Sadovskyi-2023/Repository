d4db7089db0e49fdb875034cba6f65e4e9409c7b
Volodymyr-Sadovskyi-2023 <139169228+Volodymyr-Sadovskyi-2023@users.noreply.github.com>
2023-08-20 11:31:18 +0300

    Create UsersRegister.cs

---
+++ Task Epic 2/CONFIDENT/UsersRegister.cs
@@ -0,0 +1,26 @@
+﻿namespace ChallengeMe.CONFIDENT;
+
+    internal static class UsersRegister
+    {
+        public static void UsersRegistry()
+        {
+            var user = new User();
+            user.SetValues("1", "2");
+            Users[0] = user;
+          
+            return;
+        }
+        public static User[] Users = new User[1];
+    }
+
+    internal class User
+    {
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public string Username { get;}
+        public string Password { get;}
+    }
