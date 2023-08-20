2af60aeae11e5ff9bfddbce9fe86d3007220b3ee
Volodymyr-Sadovskyi-2023 <139169228+Volodymyr-Sadovskyi-2023@users.noreply.github.com>
2023-08-20 20:51:20 +0300

    Add files via upload

---
+++ Task Epic 2.1/Services/Displayer.cs
@@ -0,0 +1,34 @@
+﻿using MythAndLegends.Data;
+using MythAndLegends.Data.Entity;
+using MythAndLegends.Services.Interface;
+
+namespace MythAndLegends.Services;
+
+public class Display : IDisplay
+{
+    private readonly IStoryService _storyService;
+
+    public Display(IStoryService storyService)
+    {
+        _storyService = storyService;
+    }
+
+    public void DisplayByCode(string code)
+    {
+        var story = _storyService.GetStoryByCode(code);
+
+        if (story is not null)
+        {
+            story.Tell();
+        }
+        else
+        {
+            Console.WriteLine($"No story with code {code}");
+        }
+    }
+
+    public void AddNewStory(Story story)
+    {
+        _storyService.AddStory(story);
+    }
+}
---
+++ Task Epic 2.1/Services/MythService.cs
@@ -0,0 +1,35 @@
+﻿using MythAndLegends.Data;
+using MythAndLegends.Data.Entity;
+using MythAndLegends.Services.Interface;
+
+namespace MythAndLegends.Services;
+
+public class MythsService : IStoryService
+{
+    private List<Myth> _myths = new List<Myth>();
+
+    public void AddStory(Story story)
+    {
+        if (story is Myth myth)
+        {
+            if (string.IsNullOrEmpty(myth.StoryCode))
+            {
+                myth.StoryCode = CreateCode(myth.Name);
+            }
+
+            _myths.Add(myth);
+        }
+    }
+
+    public Story? GetStoryByCode(string code)
+    {
+        return _myths.FirstOrDefault(myth => myth.StoryCode == code);
+    }
+
+    private string CreateCode(string name)
+    {
+        var code = $"{name.First()}{name.Last()}-{name.Length}";
+
+        return code;
+    }
+}
