5ae510b348c44240ae03634b8acdfe8acc18b706
Volodymyr-Sadovskyi-2023 <139169228+Volodymyr-Sadovskyi-2023@users.noreply.github.com>
2023-08-20 20:12:28 +0300

    Create UserEnter.cs

---
+++ Task Epic 2/UserServices/UserEnter.cs
@@ -0,0 +1,22 @@
+using ChallengeMe.CONFIDENT;
+
+namespace ChallengeMe.Tests
+{
+    public class UserEnter : UserService
+    {
+        public void EnterTheRegister(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterUser(string username, string password)
+        {
+            UsersRegister.Users.Append(new User());
+        }
+
+        public void UnregisterUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
