f8da19795cbbc5f26af797a90db3e3206fa058ba
Volodymyr-Sadovskyi-2023 <139169228+Volodymyr-Sadovskyi-2023@users.noreply.github.com>
2023-08-20 20:50:33 +0300

    Add files via upload

---
+++ Task Epic 2.1/Data/Entity/StoryRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace MythAndLegends.Data.Entity;
+
+public class StoryRepository
+{
+    private List<Story> _stories = new List<Story>();
+
+    public void AddStory(Story story)
+    {
+        _stories.Add(story);
+    }
+
+    public Story? GetStoryByCode(string code)
+    {
+        return _stories.FirstOrDefault(story => story.StoryCode == code);
+    }
+}
