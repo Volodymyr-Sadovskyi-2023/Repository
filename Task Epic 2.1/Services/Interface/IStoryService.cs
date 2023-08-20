5fbb48211fc8098b8fd0dbd25335d241deeaf799
Volodymyr-Sadovskyi-2023 <139169228+Volodymyr-Sadovskyi-2023@users.noreply.github.com>
2023-08-20 20:51:56 +0300

    Add files via upload

---
+++ Task Epic 2.1/Services/Interface/IStoryService.cs
@@ -0,0 +1,9 @@
+﻿using MythAndLegends.Data.Entity;
+
+namespace MythAndLegends.Services.Interface;
+
+public interface IStoryService
+{
+    void AddStory(Story story);
+    Story? GetStoryByCode(string code);
+}
