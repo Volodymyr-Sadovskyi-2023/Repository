36f85562abf1e101ed380688cd893d3b8fbd6cd9
Volodymyr-Sadovskyi-2023 <139169228+Volodymyr-Sadovskyi-2023@users.noreply.github.com>
2023-08-20 11:35:53 +0300

    Delete UserRegisterInterface.cs

--- Task Epic 2/UserRegisterInterface.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChallengeMe.Tests
-{
-    public interface IUserService
-    {
-        void RegisterUser(string username, string password);
-        void EnterTheRegister(string username, string password);
-        void UnregisterUser(string username);
-    }
-}
