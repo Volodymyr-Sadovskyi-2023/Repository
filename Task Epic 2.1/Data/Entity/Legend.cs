4f6c3dc03143ad23c4dcd7a673b03d56eec0f203
Volodymyr-Sadovskyi-2023 <139169228+Volodymyr-Sadovskyi-2023@users.noreply.github.com>
2023-08-22 12:44:16 +0300

    Add files via upload

---
+++ Task Epic 2.1/Data/Entity/Legend.cs
@@ -0,0 +1,14 @@
+﻿namespace MythAndLegends.Data.Entity;
+
+public class Legend : Story
+{
+    public string Object { get; set; }
+
+    public override string Tell()
+    {
+        return $"Legend {StoryCode} - {Name}\n" +
+               $"Legend tells about {Object}\n" +
+               $"{Content}\n" +
+               base.Tell();
+    }
+}
