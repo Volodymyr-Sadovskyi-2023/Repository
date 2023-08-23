df1cdb6583c00df062c230c96220f8463ab302fb
Volodymyr-Sadovskyi-2023 <139169228+Volodymyr-Sadovskyi-2023@users.noreply.github.com>
2023-08-23 16:46:45 +0300

    Update UserRegister.cs

--- Task Epic 2/UserInfo/UserRegister.cs
+++ Task Epic 2/UserInfo/UserRegister.cs
@@ -12,15 +12,3 @@
         }
         public static List<User> Users = new List<User>();
     }
-
-    internal class User
-    {
-        public User(string username, string password)
-        {
-            Username = username;
-            Password = password;
-        }
-
-        public string Username { get;}
-        public string Password { get;}
-    }
