31a6a44ea0e489a483d4c66ce2f0052092347421
Volodymyr-Sadovskyi-2023 <139169228+Volodymyr-Sadovskyi-2023@users.noreply.github.com>
2023-09-01 03:16:41 +0300

    Add files via upload

---
+++ Task Epic 2/UserServices/Data/FakingUserData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeMe.Data
+{
+    public class FakingUserData
+    {
+        public List<User> Users = new List<User>();
+
+        public FakingUserData()
+        {
+            Users.Add(new User("Beer", "password"));
+        }
+    }
+}
---
+++ Task Epic 2/UserServices/Infrastructure/Interfaces/IRegisterService.cs
@@ -0,0 +1,11 @@
+﻿namespace ChallengeMe.Infrastructure.Interfaces
+{
+    public interface IRegisterService
+    {
+        void RegisterUser(string username, string password);
+
+        void DeleteUser(string username);
+
+        void Login(string username, string password);
+    }
+}
