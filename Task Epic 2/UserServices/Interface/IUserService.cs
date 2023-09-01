5f3a1ae8e192099165532706e28c02021db459c9
Volodymyr-Sadovskyi-2023 <139169228+Volodymyr-Sadovskyi-2023@users.noreply.github.com>
2023-09-01 03:15:25 +0300

    Delete Task Epic 2/UserServices/Interface/IUserService.cs

--- Task Epic 2/UserServices/Interface/IUserService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChallengeMe.Tests
-{
-    public interface IUserService
-    {
-        void RegisterUser(string username, string password);
-        void CheckRegisterUser(string username, string password);
-        void FirstRegisterUser(string username);
-    }
-}
