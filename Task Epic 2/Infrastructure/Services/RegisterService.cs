a492f360478f64c47774a25703bc53bf02742cc9
Volodymyr-Sadovskyi-2023 <139169228+Volodymyr-Sadovskyi-2023@users.noreply.github.com>
2023-09-01 03:19:00 +0300

    Add files via upload

---
+++ Task Epic 2/Infrastructure/Services/RegisterService.cs
@@ -0,0 +1,45 @@
+﻿using ChallengeMe.Data;
+using ChallengeMe.Infrastructure.Interfaces;
+using System.Threading.Channels;
+
+namespace ChallengeMe.Infrastructure.Services
+{
+    public class RegisterService : IRegisterService
+    {
+        private FakingUserData _userData = new FakingUserData();
+        public void RegisterUser(string username, string password)
+        {
+            Console.WriteLine($"something like registration-{username}-{password}");
+        }
+
+
+        public void DeleteUser(string username)
+        {
+            var user = _userData.Users.Where(x => x.Username == username).FirstOrDefault();
+
+            if (user != null) 
+            {
+                _userData.Users.Remove(user);
+            }
+            else
+            {
+                Console.WriteLine("User not found");
+            }
+        }
+
+        public void Login(string username, string password)
+        {
+            var user = _userData.Users.FirstOrDefault(u => u.Username == username && u.Password == password);
+
+            if (user != null)
+            {
+                Console.WriteLine("You have entered " + user.Username);
+            }
+            else
+            {
+                Console.WriteLine("You have not registered");
+            }
+        }
+    }
+}
+
